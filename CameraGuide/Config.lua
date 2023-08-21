8c69875df65d40467764debafe9b65ca475ccb3d
Siu4EV3R <117762380+Siu4EV3R@users.noreply.github.com>
2023-08-22 00:29:13 +0200

    Add files via upload

---
+++ CameraGuide/Config.lua
@@ -0,0 +1,34 @@
+--░░██╗██╗███████╗██╗░░░██╗██████╗░██████╗░
+--░██╔╝██║██╔════╝██║░░░██║╚════██╗██╔══██╗
+--██╔╝░██║█████╗░░╚██╗░██╔╝░█████╔╝██████╔╝
+--███████║██╔══╝░░░╚████╔╝░░╚═══██╗██╔══██╗
+--╚════██║███████╗░░╚██╔╝░░██████╔╝██║░░██║
+--░░░░░╚═╝╚══════╝░░░╚═╝░░░╚═════╝░╚═╝░░╚═╝
+
+Config = {}
+
+Config.TimeSec = 5 -- Timer per camera
+
+Config.FirstCam = vector3(-501.07, -285.46, 82.27) -- Camera coords
+Config.FirstCamRot = {x = -35.0, y = 0.0, z = 30.0} -- Camera Rotation
+
+Config.SecondCam = vector3(-1851.65, -397.64, 51.12) -- Camera coords
+Config.SecondCamRot = {x = 0.0, y = 0.0, z = 2.83} -- Camera Rotation
+
+Config.ThirdCam = vector3(-253.68, -1884.35, 27.74) -- Camera coords
+Config.ThirdCamRot = {x = 30.0, y = 0.0, z = 136.06} -- Camera Rotation
+
+Config.FourthCam = vector3(808.62, 44.37, 120.06) -- Camera coords
+Config.FourthCamRot = {x = -10.0, y = 0.0, z = 266.45} -- Camera Rotation
+
+Config.BackSpawn = vector4(309.08, -906.77, 29.27, 73.70) -- Where you spawn back after the Guide
+
+Config.MarkerLoc = vector3(309.08, -906.77, 29.27) -- Marker Location
+
+Config.Notifs = {
+    [1] = "Tekst number 1",
+    [2] = "~y~Tekst number 2",
+    [3] = "~g~Tekst number 3",
+    [4] = "~r~Tekst number 4",
+    [5] = "~o~Tekst number 5",
+}
---
+++ CameraGuide/fxmanifest.lua
@@ -0,0 +1,18 @@
+--░░██╗██╗███████╗██╗░░░██╗██████╗░██████╗░
+--░██╔╝██║██╔════╝██║░░░██║╚════██╗██╔══██╗
+--██╔╝░██║█████╗░░╚██╗░██╔╝░█████╔╝██████╔╝
+--███████║██╔══╝░░░╚████╔╝░░╚═══██╗██╔══██╗
+--╚════██║███████╗░░╚██╔╝░░██████╔╝██║░░██║
+--░░░░░╚═╝╚══════╝░░░╚═╝░░░╚═════╝░╚═╝░░╚═╝
+
+fx_version 'bodacious' 
+game 'gta5'
+
+author 'Sinan Abi#6391'
+description 'Camera Guide script by Sinan Abi'
+-- stuur me dm in discord als je wat wilt vragen of in call wilt.
+
+client_scripts {
+    'client.lua',
+    'Config.lua'
+}
